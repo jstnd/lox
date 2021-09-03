ba42e3c002da21e5d7650e6ec630bef6da26ff86
Justin Doornbos <doornbosjustin@gmail.com>
2021-09-03 19:19:16 -0400

    chapter 4 section 6 (Longer Lexemes) complete

--- plox/scanner.py
+++ plox/scanner.py
@@ -42,16 +42,29 @@ class Scanner:
             case "=": self.add_token(TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL)
             case "<": self.add_token(TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS)
             case ">": self.add_token(TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER)
-            case _: self.interpreter.error(self.line, "Unexpected character.")
+            case "/":
+                if self.match("/"):  # COMMENT
+                    while self.peek() != "\n" and not self.at_end():
+                        self.advance()
+                else:
+                    self.add_token(TokenType.SLASH)
+            case " " | "\r" | "\t": pass  # IGNORE WHITESPACE
+            case "\n": self.line += 1
+            case '"': self.string()
+            case _:
+                if self.is_digit(c):
+                    self.number()
+                else:
+                    self.interpreter.error(self.line, "Unexpected character.")
+
+    def add_token(self, type: TokenType, literal: Any = None) -> None:
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(type, text, literal, self.line))
 
     def advance(self) -> str:
         c = self.source[self.current]
         self.current += 1
         return c
-
-    def add_token(self, type: TokenType, literal: Any = None) -> None:
-        text = self.source[self.start:self.current]
-        self.tokens.append(Token(type, text, literal, self.line))
 
     def match(self, expected: str) -> bool:
         if self.at_end() or self.source[self.current] != expected:
@@ -59,3 +72,48 @@ class Scanner:
 
         self.current += 1
         return True
+
+    def peek(self) -> str:
+        if self.at_end():
+            return "\0"
+
+        return self.source[self.current]
+
+    def peek_next(self) -> str:
+        if self.current + 1 >= len(self.source):
+            return "\0"
+
+        return self.source[self.current + 1]
+
+    def is_digit(self, c: str) -> bool:
+        return "0" <= c <= "9"
+
+    def string(self) -> None:
+        while self.peek() != '"' and not self.at_end():
+            if self.peek() == "\n":
+                self.line += 1
+
+            self.advance()
+
+        if self.at_end():
+            self.interpreter.error(self.line, "Unterminated string.")
+            return
+
+        # closing "
+        self.advance()
+
+        # trim surrounding quotes
+        value = self.source[self.start + 1:self.current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def number(self) -> None:
+        while self.is_digit(self.peek()):
+            self.advance()
+
+        if self.peek() == "." and self.is_digit(self.peek_next()):
+            self.advance()  # consume "."
+
+            while self.is_digit(self.peek()):
+                self.advance()
+
+        self.add_token(TokenType.NUMBER, float(self.source[self.start:self.current]))
