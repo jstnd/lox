5cbf52e135a7ffbadca8ea33e14fb4715e1f127d
Justin Doornbos <doornbosjustin@gmail.com>
2021-09-03 16:41:29 -0400

    chapter 4 section 1 complete

---
+++ plox/lox.py
@@ -0,0 +1,42 @@
+import sys
+
+
+class Lox:
+    def __init__(self):
+        self.had_error = False
+
+    def run(self, pgm: str):
+        scanner = Scanner(pgm)
+        tokens = scanner.scan_tokens()
+
+        for token in tokens:
+            print(token)
+
+    def run_prompt(self):
+        while True:
+            line = input("> ")
+            if not line:
+                break
+            self.run(line)
+            self.had_error = False
+
+    def error(self, line: int, message: str):
+        self.report(line, "", message)
+
+    def report(self, line: int, where: str, message: str):
+        print(f"[line {line}] Error{where}: {message}")
+        self.had_error = True
+
+
+if __name__ == "__main__":
+    lox = Lox()
+
+    if len(sys.argv) > 1:
+        print("Usage: plox [script]")
+        sys.exit(64)
+    elif len(sys.argv) == 1:
+        lox.run(open(sys.argv[1]).read())
+        if lox.had_error:
+            sys.exit(65)
+    else:
+        lox.run_prompt()
